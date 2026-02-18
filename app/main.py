f0eb87f5edfec746fe24e2adf4a46ca9042c14d9
Ronny Brenes <rjba08@gmail.com>
2026-02-17 18:06:13 -0600

    Add Question model and table test get

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ def test_db():
         return {"database": "connected"}
 
 
-@app.post("/save-test")
+@app.get("/save-test")
 def save_test():
     db = SessionLocal()
     new_question = Question(
